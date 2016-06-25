b9bdb3304e71a5e3d989d1e48985253c2b9e8124
Teac_j <teacjavaee@gmail.com>
2016-06-25 17:10:55 +0300

    Initial commit

---
+++ app/src/main/java/ru/rostovenergo/geeis/GeeisActivity.java
@@ -0,0 +1,13 @@
+package ru.rostovenergo.geeis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GeeisActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_geeis);
+    }
+}
